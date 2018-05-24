7d80d0ce74fedede596adefa51524854a0ea3682
okabe-san <aokabe@developers.wyyerd.com>
2018-05-24 16:24:15 -0600

    add section components

---
+++ src/components/sections/links.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modeling',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  height: 400px;
+  background: gray;
+}
+</style>
---
+++ src/components/sections/modeling.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2></h2>
+    <div class="container">
+      <div class="image">
+      </div>
+      <div class="image">
+      </div>
+      <div class="image">
+      </div>
+      <div class="image">
+      </div>
+      <div class="image">
+      </div>
+      <div class="image">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modeling',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 2rem;
+}
+.image {
+  box-sizing: border-box;
+  width: calc(100% /3);
+  height: 200px;
+  border: 1px solid gray;
+}
+</style>
