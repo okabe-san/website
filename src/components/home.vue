01aedb9852aceffd4725e15a1add96f262f75abe
okabe-san <aokabe@developers.wyyerd.com>
2018-05-24 14:47:26 -0600

    add links component

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,40 +1,11 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="http://3dvisualizationtips.blogspot.com/"
-          target="_blank">
-          Blog
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://www.turbosquid.com/Search/Artists/3d-vis?referral=3d-vis"
-          target="_blank">
-          3d props
-        </a>
-      </li>
-      <li>
-        <p>A single page application which uses AngularJS controllers and services. It is an everyday brain training game for everyone.
-        * To design only for a mobile *
-        </p>
-        <a
-          href="https://github.com/okabe-san/ten-puzzle-front"
-          target="_blank">
-          https://github.com/okabe-san/ten-puzzle-front
-        </a>
-        <a
-          href="https://ten-puzzle.firebaseapp.com/#/"
-          target="_blank">
-          https://ten-puzzle.firebaseapp.com
-        </a>
-      </li>
-      <li>
-      </li>
-    </ul>
+  <div>
+    <header>
+      <h1>{{ msg }}</h1>
+    </header>
+    <content>
+
+    </content>
   </div>
 </template>
 
@@ -51,4 +22,11 @@ export default {
 
 <style scoped>
 @import '../assets/style/main.css';
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 500px;
+  background: rgba(0, 0, 0, 0.1);
+}
 </style>
